60d15d1d9cc686bf5e2fce6608b1bb808a3e6c01
konstanZX2 <zxgenerationzx@gmail.com>
2022-04-29 18:38:43 +0200

    #GrammarNazi

--- src/TransformaImagen.java
+++ src/TransformaImagen.java
@@ -9,23 +9,39 @@ import java.io.IOException;
 public class TransformaImagen {
 
     File f = null;
-
     public TransformaImagen(File imagen) {
-        boolean comprobacionDeQueContieneLaExtensionBmpVerdadero = imagen.getAbsolutePath().contains(".bmp");
-        if (imagen.exists() && comprobacionDeQueContieneLaExtensionBmpVerdadero) {
-            f = imagen;
-        }
+
 
         // Control de existencia del fichero y control de la extensión .bmp (sacar
-
-        // mensajes de error)
+        boolean comprobacionDeQueContieneLaExtensionBmpVerdadero = imagen.getName().contains(".bmp");
+        if (imagen.exists() && comprobacionDeQueContieneLaExtensionBmpVerdadero) {
+            f = imagen;
+        }// mensajes de error)
+        else{
+            System.out.println("El archivo no existe o no tiene la extension .bmp");}
 
     }
 
-    public void transformaNegativo() throws IOException {
-
+    public void transformaNegativo() {
         // Transformar a negativo y guardar como *_n.bmp
+        try {   FileInputStream fileInputStream = new FileInputStream(f);
+            FileOutputStream fileOutputStream = new FileOutputStream("Resources/"+getNombreSinExtension()+"_n.bmp");
+            byte[] buffer = new byte[54];
+            fileInputStream.read(buffer);
+            fileOutputStream.write(buffer);
+            int numeroDelByte= 0;
 
+            while (numeroDelByte != -1){
+                numeroDelByte=fileInputStream.read();
+                fileOutputStream.write(255-numeroDelByte);
+
+            }
+            fileInputStream.close();
+            fileOutputStream.close();
+
+        } catch (IOException e) {
+            System.out.println("ERROOOR");;
+        }
     }
 
     public void transformaOscuro() throws IOException {
@@ -42,9 +58,13 @@ public class TransformaImagen {
 
     }
 
-    private String getNombreSinExtension() {
+  private String getNombreSinExtension() {
 
         //Devuelve el nombre del archivo f sin extensión
+      String[] partes = f.getName().split("\\.bmp");
+      return partes[0];
+
+
 
 
     }
